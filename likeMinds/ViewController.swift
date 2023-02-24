216737c021c9d005b12c5905e2d049e096adf647
ayreandangerfield <123313740+ayreandangerfield@users.noreply.github.com>
2023-02-24 14:34:54 -0600

    Initial Commit

---
+++ likeMinds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  likeMinds
+//
+//  Created by Ayrean Dangerfield on 2/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
